01359aa4ba95755c294e51983d22a7cda1d5ac3b
Claudio Poli <masterkain@gmail.com>
2010-02-03 23:54:07 +0100

    more wirecard work

--- lib/active_merchant/billing/gateways/wirecard.rb
+++ lib/active_merchant/billing/gateways/wirecard.rb
@@ -145,7 +145,7 @@ module ActiveMerchant #:nodoc:
         request = build_request(:purchase, money, @options)
         commit(request)
       end
-      
+
       # 6.11 Reversal.
       def void(authorization, options = {})
         prepare_options_hash(options)
@@ -201,9 +201,9 @@ module ActiveMerchant #:nodoc:
               xml.tag! 'W_JOB' do
                 # TODO: OPTIONAL, check what value needs to be insert here.
                 xml.tag! 'JobID', 'test dummy data'
-                # UserID for this transaction.
+                # This is the unique merchant identifier against which the request is made.
                 xml.tag! 'BusinessCaseSignature', options[:signature] || options[:login]
-                # Create the whole rest of the message.
+                # Create the whole rest of this message.
                 add_transaction_data(xml, action, money, options)
               end
             end
@@ -237,23 +237,37 @@ module ActiveMerchant #:nodoc:
           options[:action] = action
           transaction_type = action.to_s.upcase
 
+          # This is a collection of transaction data elements and their values.
           xml.tag! "FNC_CC_#{transaction_type}" do
             # This ID is reserved for merchant system data and can be used for
             # tracking purposes. Although it is optional meaning that it does
             # not have to contain data, the element itself (<FunctionID> </FunctionID>)
             # must still be provided in the XML request.
             # Omitting the element will result in a response error.
+            # Go easy with ASCII characters here.
             xml.tag! 'FunctionID', options[:description] || 'No FunctionID set'
-
+            # This is a collection of transaction data elements and their values.
             xml.tag! 'CC_TRANSACTION' do
+              # This is a unique ID associated with a single transaction, which
+              # is created by the merchant and submitted as part of the request.
+              # Mandatory.
               xml.tag! 'TransactionID', options[:order_id]
+              # The default setting of this element is eCommerce. If you like
+              # to have your CommerceType set to MOTO or CustomerPresent,
+              # please contact Wirecard support to have these options activated.
+              # Possible values:
+              #   'eCommerce'
+              #   'MOTO'
+              #   'CustomerPresent'
+              xml.tag! 'CommerceType', options[:commerce_type] || 'eCommerce'
 
-              if [:authorization, :purchase, :authorization_check].include?(action)
-                add_payment_informations(xml, money, options)
-                add_credit_card(xml, options[:credit_card])
-                add_billing_address(xml, options[:billing_address])
-              elsif action == :capture_authorization || action == :reversal
-                xml.tag! 'GuWID', options[:authorization] if options[:authorization]
+              case action
+                when :authorization, :purchase, :authorization_check
+                  add_payment_informations(xml, money, options)
+                  add_credit_card(xml, options[:credit_card])
+                  add_billing_address(xml, options[:billing_address])
+                when :capture_authorization, :reversal
+                  xml.tag! 'GuWID', options[:authorization] if options[:authorization]
               end
 
             end
@@ -262,8 +276,31 @@ module ActiveMerchant #:nodoc:
 
         # Includes the payment (amount, currency, options) to the transaction XML.
         def add_payment_informations(xml, money, options)
+          # This is the integer amount, defined in the smallest currency unit,
+          # for which the transaction is requested (e.g., $10.00 = 1000).
+          # The <Amount> element is mandatory for ‘Single’ and ‘Initial’ transaction
+          # types and if the amount of a ‘Repeated’ (recurring) transaction
+          # differs from the amount of the related ‘Initial’ transaction.
+          # For all other recurring transactions, this element is optional.
           xml.tag! 'Amount', amount(money)
+          
+          # This is the ISO 4217 currency code used for the transaction.
+          # It is mandatory if the type of transaction is ‘Single’ or ‘Initial’
+          # or if the currency of a ‘Repeated’ transaction differs from the currency
+          # of the related ‘Initial’ transaction.
           xml.tag! 'Currency', options[:currency] || currency(money)
+
+          # This is the ISO 3166-1 code of the country where the transaction takes place.
+          # It is mandatory if the type of transaction is 'Single' or 'Initial'.
+          xml.tag! 'CountryCode', options[:billing_address][:country]
+
+          # This is the field, which is shown on the customer’s card statement
+          # and can be used by the merchant for reference purposes. This feature
+          # is not supported by all the acquirers. The size of this field depends
+          # on the acquirer. Please contact Wirecard technical support for further clarification.
+          # In essence keep it short.
+          xml.tag! 'Usage', options[:usage] if options[:usage]
+
           # A recurring transaction describes a payment where the cardholder's
           # account is periodically charged for a repeated delivery and use of
           # a product or service (subscription, membership fee, etc.) over time.
@@ -275,21 +312,20 @@ module ActiveMerchant #:nodoc:
           # "+Repeated+" message (which can be another Authorization, or a Capture or a Purchase)
           # simply references an identifier (the Global Unique Wirecard ID)
           # which is returned with the response message to the initial request.
-
-          # This is the ISO 3166-1 code of the country where the transaction takes place.
-          # It is mandatory if the type of transaction is 'Single' or 'Initial'.
-          xml.tag! 'CountryCode', options[:billing_address][:country]
-
           # Recurring transaction types:
           #   :purchase
           #   :authorization
           #   :preauthorization
+          # This is a collection of recurrent information which simplifies
+          # the payment transaction message exchange between merchant and Wirecard.
+          # A Recurring Transaction is one that is authorized once by the cardholder
+          # for a repeated transaction by the merchant (e.g. monthly membership).
+          # This collection must be provided if the transaction is ‘Initial’ or ‘Repeated’.
           xml.tag! 'RECURRING_TRANSACTION' do
             # Recurring options:
-            #   +'Initial'+
             #   +'Single'+
+            #   +'Initial'+
             #   +'Repeated'+
-
             # NOTE: If the payment card data of a customer has changed, all
             # data must be re-submitted in form of an 'initial' transaction.
             # The system will generate a new reference GuWID which must be used
@@ -301,19 +337,32 @@ module ActiveMerchant #:nodoc:
         # Includes the credit card data to the transaction XML.
         def add_credit_card(xml, credit_card)
           raise "Credit card must be supplied!" if credit_card.nil?
+          # This is a collection of credit card data. It is mandatory if the
+          # type of transaction is ‘Single’ or ‘Initial’.
           xml.tag! 'CREDIT_CARD_DATA' do
+            # This is a card number against which purchase is made.
             xml.tag! 'CreditCardNumber', credit_card.number
+            # The 3- or 4-digit security code (called CVC2, CVV2 or CID depending
+            # on the card brand) that appears on the back of a credit card following
+            # the credit card number. This code does not appear on imprints.
             xml.tag! 'CVC2', credit_card.verification_value
+            # The expiry year for the card against which the purchase will be made.
             xml.tag! 'ExpirationYear', credit_card.year
+            # The expiry month for the card against which the purchase will be made.
             xml.tag! 'ExpirationMonth', format(credit_card.month, :two_digits)
+            # Any person who opens a card account and makes purchases using a card.
             xml.tag! 'CardHolderName', [credit_card.first_name, credit_card.last_name].join(' ')
+            # TODO: require CardStartYear, CardStartMonth, CardIssueNumber for Switch/Solo/Maestro.
           end
         end
 
         # Includes the IP address of the customer to the transaction XML.
         def add_customer_data(xml, options)
           return unless options[:ip]
+          # This is the collection of the contact information.
           xml.tag! 'CONTACT_DATA' do
+            # This is the IP address of the end user making the purchase.
+            # It must be provided in dot-decimal notation consisting of up to 15 characters in length.
             xml.tag! 'IPAddress', options[:ip]
           end
         end
@@ -321,24 +370,44 @@ module ActiveMerchant #:nodoc:
         # Includes the address to the transaction XML.
         def add_billing_address(xml, address)
           return if address.nil?
+          # This is a collection of risk management related elements and values.
+          # This request level along with the related elements listed below are
+          # mandatory if the type of transaction is ‘Single’ or ‘Initial’ and
+          # additionally the card transaction process is to include a risk validation.
           xml.tag! 'CORPTRUSTCENTER_DATA' do
+            # This is a collection of cardholder’s billing address elements and values.
+            # It is highly recommended to provide these elements.
+            # This element is mandatory if the CORPTRUSTCENTER_DATA level is to
+            # be included in the XML request.
             xml.tag! 'ADDRESS' do
+              # This is the first address field of the cardholder.
+              # It is recommended to enter the street name in this field.
+              # Mandatory.
               xml.tag! 'Address1', address[:address1]
+              # This is the second address field of the cardholder.
+              # It is recommended to enter the street number in this field.
+              # Optional.
               xml.tag! 'Address2', address[:address2] if address[:address2]
+              # This field shows the city associated with the cardholder.
               xml.tag! 'City', address[:city]
+              # This field shows the cardholder’s zip code.
               xml.tag! 'ZipCode', address[:zip]
-
+              # This is the state code is associated with the cardholder’s credit card.
+              # It must be provided only by US and Canadian merchants accept payments
+              # from US or Canadian residents.
               if address[:state] =~ /[A-Za-z]{2}/ && address[:country] =~ /^(us|ca)$/i
                 xml.tag! 'State', address[:state].upcase
               end
-
+              # This is the ISO 3166-1 country code associated with the cardholder.
               xml.tag! 'Country', address[:country]
+              # This is the cardholder’s phone number.
               xml.tag! 'Phone', address[:phone] if address[:phone] =~ VALID_PHONE_FORMAT
+              # This is the cardholder’s email address.
               xml.tag! 'Email', address[:email]
+              # There is also PERSONINFO but avoiding it for now.
             end
           end
         end
-
 
         # Read the XML message from the gateway and check if it was successful,
         # and also extract required return values from the response.
